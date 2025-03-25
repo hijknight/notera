895dc364fa60e25a519f5b39234e1f21d8516516
hijknight <navybluehowboutyou@gmail.com>
2025-03-24 23:26:02 -0500

    Add support for enhanced summary sources and todo list formatting
    Introduced `source` field to `Summary` and `Transcript` structs to differentiate input origins like file, note, text, and transcript. Added new methods for handling and formatting todo lists from plain text and files into markdown. Minor improvements include making `from_transcript` private and refining helper functions.

--- src/ai.rs
+++ src/ai.rs
@@ -8,7 +8,9 @@ const ENV_VAR: &str = "OPENAI_API_KEY";
 const MODEL: &str = "gpt-4o-mini";
 const URL: &str = "https://api.openai.com/v1/chat/completions";
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct Summary {
+    source: String,
     pub content: String,
 }
 
@@ -32,11 +34,11 @@ impl Summary {
         "messages": [
             {
                 "role": "system",
-                "content": "You are an ai bot that summarizes given text file content based on the content, assume that the person still needs to learn something, so don't over summarize."
+                "content": "You are an ai bot that summarizes a given text file content based on the content, assume that the person still needs to learn something, so don't over summarize."
             },
             {
                 "role": "user",
-                "content": format!("Summarize this lecture: {}", file_contents)
+                "content": format!("Summarize this text file: {}", file_contents)
             }
         ]
     });
@@ -64,6 +66,7 @@ impl Summary {
             .and_then(|content| content.as_str())
         {
             Ok(Summary {
+                source: "file".to_string(),
                 content: completion.to_string(),
             })
         } else {
@@ -124,6 +127,7 @@ impl Summary {
             .and_then(|content| content.as_str())
         {
             Ok(Summary {
+                source: "note".to_string(),
                 content: completion.to_string(),
             })
         } else {
@@ -180,6 +184,7 @@ impl Summary {
             .and_then(|content| content.as_str())
         {
             Ok(Summary {
+                source: "text".to_string(),
                 content: completion.to_string(),
             })
         } else {
@@ -187,7 +192,7 @@ impl Summary {
         }
     }
 
-    pub async fn from_transcript(transcript: &Transcript) -> Result<Self, NoteraError> {
+    async fn from_transcript(transcript: &Transcript) -> Result<Self, NoteraError> {
         let client = Client::new();
 
         let openai_api_key = env::var(ENV_VAR)
@@ -235,6 +240,7 @@ impl Summary {
             .and_then(|content| content.as_str())
         {
             Ok(Summary {
+                source: "transcript".to_string(),
                 content: completion.to_string(),
             })
         } else {
@@ -243,14 +249,137 @@ impl Summary {
 
     }
 
+    pub async fn from_list_text(list_text: &str) -> Result<Self, NoteraError> {
+        let client = Client::new();
+
+        let openai_api_key: String = env::var(ENV_VAR)
+            .unwrap_or_else(|_| {
+                println!("Please set the {} environment variable", ENV_VAR);
+                "No api key found".to_string()
+            });
+
+        let body = json!({
+        "model": MODEL,
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are an ai bot is given todo list based on the content, helping to make the todo list nicer and formatted in raw markdown for someone to copy and paste into their file."
+            },
+            {
+                "role": "user",
+                "content": format!("Make my todo list nicer: {}", list_text)
+            }
+        ]});
+
+        let mut sp = Spinner::new(Spinners::Dots9, "Sending text to ai (may take some time)".into());
+
+        let response = client.post(URL)
+            .header("Authorization", format!("Bearer {}", openai_api_key))
+            .json(&body)
+            .send()
+            .await
+            .map_err(|err| NoteraError::Reqwest(err))?;
+
+        sp.stop();
+
+        let response_text = response.text().await
+            .map_err(|err| NoteraError::Reqwest(err))?;
+
+        let json: Value = serde_json::from_str(&response_text)
+            .map_err(|err| NoteraError::SerdeJson(err))?;
+
+        println!("\n");
+
+        if let Some(completion) = json.get("choices")
+            .and_then(|choices| choices.get(0))
+            .and_then(|choice| choice.get("message"))
+            .and_then(|message| message.get("content"))
+            .and_then(|content| content.as_str())
+        {
+            Ok(Summary {
+                source: "text".to_string(),
+                content: completion.to_string(),
+            })
+        } else {
+            Err(NoteraError::AI("Unable to find choice message".to_string()))
+        }
+    }
+
+    pub async fn from_list_file(file_path: &str) -> Result<Self, NoteraError> {
+        let client = Client::new();
+
+        let file_contents = fs::read_to_string(file_path)
+            .map_err(|err| NoteraError::FileSystem(err, None))?;
+
+        let openai_api_key: String = env::var(ENV_VAR)
+            .unwrap_or_else(|_| {
+                println!("Please set the {} environment variable", ENV_VAR);
+                "No api key found".to_string()
+            });
+
+        let mut sp = Spinner::new(Spinners::Dots9, "Sending text file to ai (may take some time)".into());
+
+        let body = json!({
+        "model": MODEL,
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are an ai bot is given todo list based on the content, helping to make the todo list nicer and formatted in raw markdown for someone to copy and paste into their file.",
+            },
+            {
+                "role": "user",
+                "content": format!("make this list nicer: {}", file_contents)
+            }
+        ]
+    });
+
+        let response = client.post(URL)
+            .header("Authorization", format!("Bearer {}", openai_api_key))
+            .json(&body)
+            .send()
+            .await
+            .map_err(|err| NoteraError::Reqwest(err))?;
+
+        sp.stop();
+
+        let response_text = response.text().await
+            .map_err(|err| NoteraError::Reqwest(err))?;
+
+        let json: Value = serde_json::from_str(&response_text).map_err(|err| NoteraError::SerdeJson(err))?;
+
+        println!("\n");
+
+        if let Some(completion) = json.get("choices")
+            .and_then(|choices| choices.get(0))
+            .and_then(|choice| choice.get("message"))
+            .and_then(|message| message.get("content"))
+            .and_then(|content| content.as_str())
+        {
+            Ok(Summary {
+                source: "file".to_string(),
+                content: completion.to_string(),
+            })
+        } else {
+            Err(NoteraError::AI("Unable to find choice message".to_string()))
+        }
+    }
+
+
     pub async fn transcribe_and_summarize(audio_file: &str) -> Result<Self, NoteraError> {
         let transcript = Transcript::from_audio(audio_file).await?;
         let summary = Summary::from_transcript(&transcript).await?;
         Ok(summary)
     }
+
+    pub fn print(&self) {
+        println!("{}", self.content);
+        println!("\n");
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Transcript {
+    pub source: String,
     pub content: String,
 }
 
@@ -297,12 +426,19 @@ impl Transcript {
         let json: Value = serde_json::from_str(&response_text).map_err(|err| NoteraError::SerdeJson(err))?;
 
         if let Some(transcript) = json.get("text") {
+            #[allow(dead_code)]
             Ok(Transcript {
+                source: "audio".to_string(),
                 content: transcript.to_string(),
             })
         } else {
             Err(NoteraError::AI("Unable to find choice message".to_string()))
         }
+    }
+
+    pub fn print(&self) {
+        println!("{}", self.content);
+        println!("\n");
     }
 }
 
