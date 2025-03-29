54203884a2b8f3cca5df2f6fc285d38bbc505c2e
hijknight <navybluehowboutyou@gmail.com>
2025-03-29 15:10:35 -0500

    Add `Note` struct and functionality for note creation
    Introduces the `Note` struct to represent notes with a title, content, and timestamp. Implements methods for creating a note via an editor, formatting its output, and printing raw details. This lays the foundation for managing notes in the application.

---
+++ src/notes.rs
@@ -0,0 +1,68 @@
+
+use chrono::Local;
+use crate::{
+    error::{
+        print_warning,
+        with_path,
+        NoteraError,
+        Result,
+    },
+    config::Config
+};
+
+use std::fs;
+
+#[derive(Debug, Clone, PartialEq)]
+
+pub struct Note {
+    pub title: String,
+    pub content: String,
+    pub timestamp: String,
+}
+
+
+
+impl Note {
+    pub fn from_editor(title: &str) -> Result<Self> {
+        let config = Config::load()?;
+        let editor = &config.editor;
+
+        let temp_dir = tempfile::tempdir()?;
+        let temp_file_path = temp_dir.path().join("notera_tmp_note.md");
+
+        let status = std::process::Command::new(editor)
+            .arg(&temp_file_path)
+            .status()
+            .map_err(|e| NoteraError::Other(format!("Failed to open editor: {}", e)))?;
+
+        if !status.success() {
+            return Err(NoteraError::Other("Editor exited with non-zero status".to_string()));
+        }
+
+        let content = fs::read_to_string(&temp_file_path)
+            .map_err(|e| with_path(e, temp_file_path))?
+            .trim()
+            .to_string();
+
+        if content.is_empty() {
+            return Err(NoteraError::Other("Note discarded (empty content)".to_string()));
+        }
+
+        let timestamp = Local::now().format("%Y-%m-%d %H:%M").to_string();
+
+        Ok(Note {
+            title: title.to_string(),
+            content,
+            timestamp,
+        })
+    }
+
+    pub fn format(&self) -> String {
+        format!("📝 Title: {}\n⏳ Created: {}\n\n{}\n", self.title, self.timestamp, self.content)
+    }
+
+    #[allow(dead_code)]
+    pub fn print_raw(&self) {
+        println!("{:#?}", self)
+    }
+}
